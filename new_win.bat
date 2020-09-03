a2d207d0901bee5fe2453d012bdfccc25469274a
Martin Ottersland <SlyOtis@users.noreply.github.com>
2020-09-03 13:24:43 -0700

    Update new_win.bat

--- new_win.bat
+++ new_win.bat
@@ -1,1 +1,9 @@
 @echo off
+choco install firefox -y
+choco install brave -y
+choco install vscode -y
+choco install vlc -y
+choco install GoogleChrome -y
+chcoc install bitward -y
+chocp install insomnia -y
+pause
